346c01f818c938f1591f4e3a92bf6747126be291
TatiSalazar <tsalazarp.uq@gmail.com>
2022-06-13 09:41:10 -0500

    Mi mensaje

--- modules/src/main/java/com/platzi/functional/_15_streams_intro/TypeStream.java
+++ modules/src/main/java/com/platzi/functional/_15_streams_intro/TypeStream.java
@@ -2,15 +2,17 @@ package com.platzi.functional._15_streams_intro;
 
 import org.w3c.dom.ls.LSOutput;
 
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
 public class TypeStream {
 
     public static void main(String[] args) {
         IntStream infiniteStream = IntStream.iterate(0,x -> x +1);
-        infiniteStream.limit(10)
-                .parallel()
+        infiniteStream.limit(1000)
                 .filter(x -> x%2 == 0)
-                .forEach(System.out::println);
+                .boxed()
+                .collect(Collectors.toList());
     }
 }
